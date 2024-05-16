6b21f6aac3d266f36dde4cd514f5b3d0b3066291
JustBen71500 <randazzobenjamin@outlook.fr>
2024-05-16 13:24:02 +0200

    3 ième Intention

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 
 # Charger le jeu de données
-df = pd.read_csv('dataset.csv')
+df = pd.read_csv('train.csv')
 
 # Afficher un échantillon du jeu de données
 print(df.head())
 
 # Séparer les données et les étiquettes
-X = df['review']
-y = df['sentiment']
+X = df['Phrase']
+y = df['Sentiment']
 
 # Diviser le jeu de données en ensembles d'entraînement et de test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -23,7 +23,7 @@ X_train_tfidf = vectorizer.fit_transform(X_train)
 X_test_tfidf = vectorizer.transform(X_test)
 
 # Créer et entraîner le modèle de régression logistique
-model = LogisticRegression()
+model = LogisticRegression(max_iter=500)
 model.fit(X_train_tfidf, y_train)
 
 # Prédire les intentions sur l'ensemble de test
@@ -35,8 +35,13 @@ print(f'Accuracy: {accuracy}')
 print(classification_report(y_test, y_pred))
 
 # Prédire l'intention pour de nouveaux commentaires
-new_comments = ["This film was really incredible", "I did not like this film"]
+new_comments = ["This film was really incredible", "I did not like this film", "This film was average"]
 new_comments_tfidf = vectorizer.transform(new_comments)
 predictions = model.predict(new_comments_tfidf)
 for comment, pred in zip(new_comments, predictions):
-    print(f'Commentaire: {comment} - Intention: {pred}')
+    if pred == 0 or pred == 1 :
+        print(f'Commentaire: {comment} - Intention: negative')
+    elif pred == 2 :
+        print(f'Commentaire: {comment} - Intention: neutral')
+    else :
+        print(f'Commentaire: {comment} - Intention: positive')
